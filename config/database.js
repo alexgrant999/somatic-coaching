d1fd25b8ccf3f98b18b1cdee2a82194fa3537a2d
Alex Grant <alex@findyoga.com.au>
2022-05-25 18:45:13 +1000

    updated database

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'somatic-coaching.ccfdcfzxhidq.ap-southeast-2.rds.amazonaws.com'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'somatic-coaching'),
+      database: env('DATABASE_NAME', 'somaticcoaching'),
       user: env('DATABASE_USERNAME', 'admin'),
       password: env('DATABASE_PASSWORD', 'rCQilnxNbdra6ZEPSogX'),
       ssl: env.bool('DATABASE_SSL', false),
